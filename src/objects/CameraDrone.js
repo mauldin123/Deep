0507eb61d7ef1db28fa1e5d7ed4646db54ed6349
periodicaidan <periodicaidan@gmail.com>
2019-09-24 20:28:34 -0500

    Added more control stuff

--- src/objects/CameraDrone.js
+++ src/objects/CameraDrone.js
@@ -1,21 +1,25 @@
 export default class CameraDrone extends Phaser.Physics.Arcade.Sprite {
 	constructor(scene, x, y) {
 		super(scene, x, y, 'camera');
+
+		this.scene.add.existing(this);
+		this.scene.physics.add.existing(this);
+		this.setScale(0.5);
 	}
 
 	update(controls) {
 		if (controls.left.isDown) {
-			this.setVelocityX(-100);
+			this.setVelocityX(-400);
 		} else if (controls.right.isDown) {
-			this.setVelocityX(100);
+			this.setVelocityX(400);
 		} else {
 			this.setVelocityX(0);
 		}
 
 		if (controls.up.isDown) {
-			this.setVelocityY(-100);
+			this.setVelocityY(-400);
 		} else if (controls.down.isDown) {
-			this.setVelocityY(100);
+			this.setVelocityY(400);
 		} else {
 			this.setVelocityY(0);
 		}
