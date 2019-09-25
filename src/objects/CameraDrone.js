7667a51ecd8d830225b33dad5d2e0dbff2d6a0c3
Chasen <cmauldin@utexas.edu>
2019-09-24 20:50:42 -0500

    added drag and other assets of camera

--- src/objects/CameraDrone.js
+++ src/objects/CameraDrone.js
@@ -1,21 +1,38 @@
 export default class CameraDrone extends Phaser.Physics.Arcade.Sprite {
 	constructor(scene, x, y) {
 		super(scene, x, y, 'camera');
+
+		this.scene.add.existing(this);
+		this.scene.physics.add.existing(this);
+		this.setScale(.33)
 	}
 
 	update(controls) {
+
+
+
 		if (controls.left.isDown) {
-			this.setVelocityX(-100);
+			this.setVelocityX(-200);
+			this.setDrag(2000);
+			this.setTexture(cameraLeft.png)
 		} else if (controls.right.isDown) {
-			this.setVelocityX(100);
+			this.setVelocityX(200);
+			this.setDrag(2000);
+			this.setAngle(0)
+
 		} else {
 			this.setVelocityX(0);
+			this.setDrag(2000);
 		}
 
 		if (controls.up.isDown) {
-			this.setVelocityY(-100);
+			this.setVelocityY(-200);
+			this.setDrag(2000);
+			this.setAngle(-90)
 		} else if (controls.down.isDown) {
-			this.setVelocityY(100);
+			this.setVelocityY(200);
+			this.setDrag(2000);
+			this.setAngle(90)
 		} else {
 			this.setVelocityY(0);
 		}
