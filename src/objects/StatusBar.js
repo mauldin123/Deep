79a9cd1364da580fc2c7ba51ffd0259b26307fce
periodicaidan <periodicaidan@gmail.com>
2019-10-20 21:47:04 -0500

    Added enemy tutorial

--- src/objects/StatusBar.js
+++ src/objects/StatusBar.js
@@ -2,5 +2,6 @@ export default class StatusBar extends Phaser.GameObjects.Sprite {
 	constructor(scene, x, y) {
 		super(scene, x, y, 'camera');
 		scene.add.rectangle(x, y, x + 100, y + 50, 0x999999);
+		scene.add.rectangle(x + 5, y + 5, x + 90, y + 5, 0xFF0000);
 	}
 }
